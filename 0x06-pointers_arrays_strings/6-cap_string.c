ee856ef79a93a6fecb4a63a907032218a1b9410e
Eyoabefrem <eyoabefrem9@gmail.com>
2023-03-29 14:43:05 -0700

    C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -13,8 +13,8 @@ while (str[index])
 while (!(str[index] >= 'a' && str[index] <= 'z'))
 index++;
 if (str[index - 1] == ' ' ||
-str[index - 1] == '\t' ||
-str[index - 1] == '\n' ||
+str[index - 1] == 't' ||
+str[index - 1] == 'n' ||
 str[index - 1] == ',' ||
 str[index - 1] == ';' ||
 str[index - 1] == '.' ||
